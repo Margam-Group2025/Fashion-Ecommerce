819507b4efec137d28d7e751928e8bad39893f5c
samikshyapattnaik <samikshyapattnaik14@gmail.com>
2025-07-25 18:12:17 +0530

    Productlisting Roter added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import Header from './components/Header'
+import Footer from './Footer/Footer';
 import Home from './pages/Home'
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import ProductListing from './pages/ProductListing';
 
 function App() {
 
   return (
     <>
-    <BrowserRouter>
-          <Header/>
-       
-     <Routes>
+      <BrowserRouter>
+          <Header/> 
+       <Routes>
         <Route path={"/"} exact={true} element={<Home />} /> 
-        
+        <Route path={'/productlisting'} exact={true} element={<ProductListing/>} >
+        </Route>
      </Routes>
+     <Footer/>
     </BrowserRouter>
      </>
   )
